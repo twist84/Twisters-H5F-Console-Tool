a725a7eb176fa6e41d1c4d4d67b0cbc72d5effcf
theTwist84 <lee.foulger.94@gmail.com>
2016-09-14 10:39:06 +0100

    New: Now launches Halo5Forge on startup if it isn't already launched.
    Changes:
    Now spits out an error instead of closing the application if invalid command is entered.

--- Twisters-H5F-Console-Tool/MemoryManager.cs
+++ Twisters-H5F-Console-Tool/MemoryManager.cs
@@ -7,12 +7,13 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Text;
 
 namespace Memory
 {
     class Manager
     {
-        public static Process process = Process.GetProcessesByName("halo5forge").FirstOrDefault();
+        public static int H5Fpid = (int)UWP.LaunchApp("Microsoft.Halo5Forge_1.114.4592.2_x64__8wekyb3d8bbwe");
 
         public enum ProcessAccessFlags : uint
         {
@@ -43,7 +44,7 @@ namespace Memory
         public static byte[] ReadFromAddress(Int32 address)
         {
             Int64 offset = Addresses.Base.ToInt64() + address;
-            var hProc = OpenProcess(ProcessAccessFlags.All, false, (int)process.Id);
+            var hProc = OpenProcess(ProcessAccessFlags.All, false, (int)H5Fpid);
             int unused = 0;
             IntPtr addr = new IntPtr(offset);
             byte[] hex = new byte[4];
@@ -54,7 +55,7 @@ namespace Memory
         public static void WriteToAddress(Int32 address, byte[] hex)
         {
             Int64 offset = Addresses.Base.ToInt64() + address;
-            var hProc = OpenProcess(ProcessAccessFlags.All, false, (int)process.Id);
+            var hProc = OpenProcess(ProcessAccessFlags.All, false, (int)H5Fpid);
             int unused = 0;
             IntPtr addr = new IntPtr(offset);
             WriteProcessMemory(hProc, addr, hex, (UInt32)hex.LongLength, out unused);
@@ -65,10 +66,78 @@ namespace Memory
 
     class Addresses
     {
-        public static IntPtr Base = Manager.process.MainModule.BaseAddress;
+        public static IntPtr Base = Process.GetProcessesByName("halo5forge").FirstOrDefault().MainModule.BaseAddress;
         public static string baseAddress = Base.ToString("X"); // Base memory address
         public static Int32 FOV = 0x58ECF90; // FOV memory address
         public static List<Int32> FPS = new List<Int32>(new Int32[] { 0x34B8C50, 0x34B8C60, 0x34B8C70 }); // FPS memory addresses
         //public static Int32 DOF = Int32.Parse(baseAddress) + ; // DOF memory address
+    }
+
+    class UWP
+    {
+        enum ActivateOptions
+        {
+            None = 0x00000000,  // No flags set
+            DesignMode = 0x00000001,  // The application is being activated for design mode
+            NoErrorUI = 0x00000002,  // Do not show an error dialog if the app fails to activate                                
+            NoSplashScreen = 0x00000004,  // Do not show the splash screen when activating the app
+        }
+
+        [ComImport, InterfaceType(ComInterfaceType.InterfaceIsIUnknown), Guid("2e941141-7f97-4756-ba1d-9decde894a3d")]
+        interface IApplicationActivationManager
+        {
+            int ActivateApplication([MarshalAs(UnmanagedType.LPWStr)] string appUserModelId, [MarshalAs(UnmanagedType.LPWStr)] string arguments,
+                ActivateOptions options, out uint processId);
+            int ActivateForFile([MarshalAs(UnmanagedType.LPWStr)] string appUserModelId, IntPtr pShelItemArray,
+                [MarshalAs(UnmanagedType.LPWStr)] string verb, out uint processId);
+            int ActivateForProtocol([MarshalAs(UnmanagedType.LPWStr)] string appUserModelId, IntPtr pShelItemArray,
+                [MarshalAs(UnmanagedType.LPWStr)] string verb, out uint processId);
+        }
+
+        [ComImport, Guid("45BA127D-10A8-46EA-8AB7-56EA9078943C")]
+        class ApplicationActivationManager { }
+
+        [DllImport("kernel32")]
+        static extern int OpenPackageInfoByFullName([MarshalAs(UnmanagedType.LPWStr)] string fullName, uint reserved, out IntPtr packageInfo);
+
+        [DllImport("kernel32")]
+        static extern int GetPackageApplicationIds(IntPtr pir, ref int bufferLength, byte[] buffer, out int count);
+
+        [DllImport("kernel32")]
+        static extern int ClosePackageInfo(IntPtr pir);
+
+        public static uint LaunchApp(string packageFullName, string arguments = null)
+        {
+            IntPtr pir = IntPtr.Zero;
+            OpenPackageInfoByFullName(packageFullName, 0, out pir);
+
+            int length = 0, count;
+            GetPackageApplicationIds(pir, ref length, null, out count);
+
+            var buffer = new byte[length];
+            GetPackageApplicationIds(pir, ref length, buffer, out count);
+
+            var appUserModelId = Encoding.Unicode.GetString(buffer, IntPtr.Size * count, length - IntPtr.Size * count);
+            var activation = (IApplicationActivationManager)new ApplicationActivationManager();
+            uint pid;
+            int hr = activation.ActivateApplication(appUserModelId, arguments ?? string.Empty, ActivateOptions.NoErrorUI, out pid);
+            if (hr < 0)
+                Marshal.ThrowExceptionForHR(hr);
+            return pid;
+        }
+    }
+
+    class Other
+    {
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        public static void SetOwnToForeground()
+        {
+            IntPtr Handle = Process.GetCurrentProcess().MainWindowHandle;
+
+            SetForegroundWindow(Handle);
+        }
     }
 }
--- Twisters-H5F-Console-Tool/Program.cs
+++ Twisters-H5F-Console-Tool/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 
 using Commands;
-//using Memory;
+using Memory;
+
+using System.Diagnostics;
 
 namespace Twisters_H5F_Console_Tool
 {
@@ -9,59 +11,86 @@ namespace Twisters_H5F_Console_Tool
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Base Address: {0}{1}", Addresses.baseAddress, Environment.NewLine);
-            Console.WriteLine("Current FOV: {0}", Get.FOV()); // Tell user current FOV
-            Console.WriteLine("Current FPS: {0}", Get.FPS()); // Tell user current FPS
-            Console.WriteLine("{0}Enter a Command or Type Help to Show help:", Environment.NewLine);
-
-            while (true) // Main loop
+            Process p = default(Process);
+            try
             {
-                string input = Console.ReadLine(); // Get input from user
+                p = Process.GetProcessById((int)Manager.H5Fpid);
+            }
+            catch { }
 
-                switch (input.ToLower()) // Switch with lower case user input
+            if ((p != null))
+            {
+                Other.SetOwnToForeground();
+                //Console.WriteLine("Base Address: {0}{1}", Addresses.baseAddress, Environment.NewLine);
+                try
                 {
-                    case "help":
-                        Console.WriteLine("Type FOV and Default or a value between 65-150,\nI.E. fov default or fov 110"); // Tell user FOV help options
-                        Console.WriteLine("Type FPS and Default or a value between 30-300,\nI.E. fps default or fps 110"); // Tell user FPS help options
-                        Console.WriteLine("Type Exit to close the application."); // Tell user Exit help options
-                        break;
-                    case "exit":
-                        Console.WriteLine("Exiting the application.");
-                        Environment.Exit(0); // Exit the appliction
-                        break;
+                    Get.FOV();
                 }
+                catch { }
 
-                string[] Input = input.Split(' '); // Split input into array
-
-                if (Input.Length.Equals(2)) // If array has 2 entries continue
+                if((Get.FOV() != 0))
                 {
-                    Int32 n;
-                    bool isNumeric = Int32.TryParse(Input[1], out n); // Check array entry 2 for int
+                    Console.WriteLine("Current FOV: {0}", Get.FOV()); // Tell user current FOV
+                }
+                else
+                    Console.WriteLine("Current FOV unavailable (game not fully loaded maybe?)");
 
-                    if (Input[0].ToLower().Equals("fov")) // If array entry 1 lower case equals "fov" continue
+                Console.WriteLine("Current FPS: {0}", Get.FPS()); // Tell user current FPS
+                Console.WriteLine("{0}Enter a Command or Type Help to Show help:", Environment.NewLine);
+
+                while (true) // Main loop
+                {
+                    string input = Console.ReadLine(); // Get input from user
+
+                    switch (input.ToLower()) // Switch with lower case user input
                     {
-                        if (isNumeric.Equals(true)) // If array entry 2 Has Int Continue
-                        {
-                            Console.WriteLine("Setting FOV to {0}.", Int32.Parse(Input[1]));
-                            Set.FOV(Int32.Parse(Input[1])); // Set FOV to array entry 2
-                        }
-                        else if (Input[1].ToLower().Equals("default")) // If array entry 2 lower case equals "default" continue
-                        {
-                            Console.WriteLine("Setting FOV back to default.");
-                            Set.FOV(); // Set FOV to default
-                        }
+                        case "help":
+                            Console.WriteLine("Type FOV and Default or a value between 65-150,\nI.E. fov default or fov 110"); // Tell user FOV help options
+                            Console.WriteLine("Type FPS and Default or a value between 30-300,\nI.E. fps default or fps 110"); // Tell user FPS help options
+                            Console.WriteLine("Type Exit to close the application."); // Tell user Exit help options
+                            break;
+                        case "exit":
+                            Console.WriteLine("Exiting the application.");
+                            Environment.Exit(0); // Exit the appliction
+                            break;
+                        default:
+                            if (!(input.ToLower().StartsWith("fov") || input.ToLower().StartsWith("fps")))
+                                Console.WriteLine("Please enter a valid command.");
+                            break;
                     }
-                    else if (Input[0].ToLower().Equals("fps")) // If array entry 1 lower case equals "fps" continue
+
+                    string[] Input = input.Split(' '); // Split input into array
+
+                    if (Input.Length.Equals(2)) // If array has 2 entries continue
                     {
-                        if (isNumeric.Equals(true)) // If array entry 2 has int continue
+                        Int32 n;
+                        bool isNumeric = Int32.TryParse(Input[1], out n); // Check array entry 2 for int
+
+                        if (Input[0].ToLower().Equals("fov")) // If array entry 1 lower case equals "fov" continue
                         {
-                            Console.WriteLine("Setting FPS to {0}.", Int32.Parse(Input[1]));
-                            Set.FPS(Int32.Parse(Input[1])); // Set FPS to array entry 2
+                            if (isNumeric.Equals(true)) // If array entry 2 Has Int Continue
+                            {
+                                Console.WriteLine("Setting FOV to {0}.", Int32.Parse(Input[1]));
+                                Set.FOV(Int32.Parse(Input[1])); // Set FOV to array entry 2
+                            }
+                            else if (Input[1].ToLower().Equals("default")) // If array entry 2 lower case equals "default" continue
+                            {
+                                Console.WriteLine("Setting FOV back to default.");
+                                Set.FOV(); // Set FOV to default
+                            }
                         }
-                        else if (Input[1].ToLower().Equals("default")) // If array entry 2 lower case equals "default" continue
+                        else if (Input[0].ToLower().Equals("fps")) // If array entry 1 lower case equals "fps" continue
                         {
-                            Console.WriteLine("Setting FPS back to default.");
-                            Set.FPS(); // Set FPS to default
+                            if (isNumeric.Equals(true)) // If array entry 2 has int continue
+                            {
+                                Console.WriteLine("Setting FPS to {0}.", Int32.Parse(Input[1]));
+                                Set.FPS(Int32.Parse(Input[1])); // Set FPS to array entry 2
+                            }
+                            else if (Input[1].ToLower().Equals("default")) // If array entry 2 lower case equals "default" continue
+                            {
+                                Console.WriteLine("Setting FPS back to default.");
+                                Set.FPS(); // Set FPS to default
+                            }
                         }
                     }
                 }
