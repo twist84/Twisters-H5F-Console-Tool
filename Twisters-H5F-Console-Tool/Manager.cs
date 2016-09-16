969eb1ad802e04aa2e544586ffe054d5b953c2e1
theTwist84 <lee.foulger.94@gmail.com>
2016-09-16 10:29:47 +0100

    Introduction of Tls, now if only I knew how to do anything with it.

--- Twisters-H5F-Console-Tool/Manager.cs
+++ Twisters-H5F-Console-Tool/Manager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Management.Automation;
 using System.Runtime.InteropServices;
@@ -9,7 +10,7 @@ using System.Threading;
 
 namespace Manager
 {
-    class Memory // Taken from https://github.com/CorpenEldorito/Corps-H5F-Tool Credit goes to Corpen.
+    class Memory // Taken from https://github.com/CorpenEldorito/Corps-H5F-Tool Credit goes to Corpen.s
     {
         public static int H5Fpid = (int)UWP.LaunchApp(UWP.GetH5FAppName());
 
@@ -28,20 +29,36 @@ namespace Manager
         }
 
         [DllImport("kernel32.dll")]
-        static extern IntPtr OpenProcess(ProcessAccessFlags dwDesiredAccess, [MarshalAs(UnmanagedType.Bool)] bool bInheritHandle, int dwProcessId);
+        static extern IntPtr OpenProcess(
+            ProcessAccessFlags dwDesiredAccess, 
+            [MarshalAs(UnmanagedType.Bool)] bool bInheritHandle, 
+            int dwProcessId
+        );
 
         [DllImport("kernel32.dll", SetLastError = true)]
-        static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out int lpNumberOfBytesWritten);
+        static extern bool WriteProcessMemory(
+            IntPtr hProcess, 
+            IntPtr lpBaseAddress, 
+            byte[] lpBuffer, 
+            uint nSize, 
+            out int lpNumberOfBytesWritten
+        );
 
         [DllImport("kernel32.dll")]
-        public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint dwSize, ref int lpNumberOfBytesRead);
+        public static extern bool ReadProcessMemory(
+            IntPtr hProcess, 
+            IntPtr lpBaseAddress, 
+            byte[] lpBuffer, 
+            uint dwSize, 
+            ref int lpNumberOfBytesRead
+        );
 
         [DllImport("kernel32.dll")]
         public static extern Int32 CloseHandle(IntPtr hProcess);
 
-        public static byte[] ReadFromAddress(Int32 address)
+        public static byte[] ReadFromAddress(Int64 BaseAddress, Int32 Address)
         {
-            Int64 offset = Addresses.Base().ToInt64() + address;
+            Int64 offset = BaseAddress + Address;
             var hProc = OpenProcess(ProcessAccessFlags.All, false, (int)H5Fpid);
             int unused = 0;
             IntPtr addr = new IntPtr(offset);
@@ -50,9 +67,9 @@ namespace Manager
             return hex;
         }
 
-        public static void WriteToAddress(Int32 address, byte[] hex)
+        public static void WriteToAddress(Int64 BaseAddress, Int32 Address, byte[] hex)
         {
-            Int64 offset = Addresses.Base().ToInt64() + address;
+            Int64 offset = BaseAddress + Address;
             var hProc = OpenProcess(ProcessAccessFlags.All, false, (int)H5Fpid);
             int unused = 0;
             IntPtr addr = new IntPtr(offset);
@@ -64,7 +81,7 @@ namespace Manager
 
     class Addresses
     {
-        public static IntPtr Base()
+        public static IntPtr getBaseAddress()
         {
             IntPtr p = default(IntPtr);
             try
@@ -75,10 +92,41 @@ namespace Manager
             
             return p;
         }
-        public static string baseAddress = Base().ToString("X");
+        public static IntPtr getTebBaseAddress()
+        {
+            IntPtr p = default(IntPtr);
+            try
+            {
+                p = (IntPtr)Tls.GetMainHaloThreadInfo().TebBaseAddress;
+            }
+            catch { }
+
+            return p;
+        }
+        public static string BaseAddress = getBaseAddress().ToString();
+        public static string TebBaseAddress = getTebBaseAddress().ToString();
         public static Int32 FOV = 0x58ECF90;
-        public static List<Int32> FPS = new List<Int32>(new Int32[] { 0x34B8C50, 0x34B8C60, 0x34B8C70 });
-        //public static Int32 DOF = Int32.Parse(baseAddress) + ;
+        public static List<Int32> FPS = new List<Int32>(new Int32[] {
+            0x34B8C50,
+            0x34B8C60,
+            0x34B8C70
+        });
+        public static List<Int64> DOF = new List<Int64>(new Int64[] {
+            0x49B0,
+            0x1B7E39C
+        });
+        public static List<Int32> ResWidth = new List<Int32>(new Int32[] {
+            0x4E97F74,
+            0x4E97F7C,
+            0x4E97F90
+            //halo5forge.g_LEngineDefaultPoolId+6B4954
+        });
+        public static List<Int32> ResHeight = new List<Int32>(new Int32[] {
+            0x4E97F78,
+            0x4E97F80,
+            0x4E97F94
+            //halo5forge.g_LEngineDefaultPoolId+6B4958
+        });
     }
 
     class UWP // Taken from http://blogs.microsoft.co.il/pavely/2015/10/24/launching-windows-store-apps-programmatically/ Credit goes to Pavel.
@@ -147,7 +195,7 @@ namespace Manager
             {
                 if (!p.Equals(null))
                 {
-                    while (Addresses.Base().Equals(0))
+                    while (Addresses.getBaseAddress().Equals(0))
                         Thread.Sleep(100);
                     while (float.Parse(Commands.Get("FOV")).Equals(0))
                         Thread.Sleep(100);
@@ -235,8 +283,8 @@ namespace Manager
                 case true:
                     if (!Input[1].ToLower().Equals(null))
                     {
-                        Int32 n;
-                        bool isNumeric = Int32.TryParse(Input[1].ToLower(), out n);
+                        Single n;
+                        bool isNumeric = Single.TryParse(Input[1].ToLower(), out n);
 
                         if (Input[0].ToLower().Equals("fov"))
                             if (Input[1].ToLower().Equals("default"))
@@ -253,6 +301,20 @@ namespace Manager
                                 Commands.Set("FPS", n);
                             else
                                 Console.WriteLine("{0} is not a valid argument for FPS, type Help for examples", Input[1]);
+                    }
+                    else
+                    {
+                        Single n,o;
+                        bool isNumericW = Single.TryParse(Input[1].ToLower().Split('x')[0], out n);
+                        bool isNumericH = Single.TryParse(Input[1].ToLower().Split('x')[0], out o);
+
+                        if (Input[0].ToLower().Equals("rw"))
+                            if (Input[1].ToLower().Equals("default"))
+                                Commands.Set("RES", 1920, 1080);
+                            else if ((isNumericW && isNumericH).Equals(true))
+                                Commands.Set("RES", n, o);
+                            else
+                                Console.WriteLine("{0} is not a valid argument for FOV, type Help for examples", Input[1]);
                     }
                     break;
                 case false:
@@ -273,20 +335,34 @@ namespace Manager
             switch (grab.ToUpper())
             {
                 case "FOV":
-                    return BitConverter.ToSingle(Memory.ReadFromAddress(Addresses.FOV), 0).ToString();
+                    return String.Format(
+                        "{0:0.00}", 
+                        BitConverter.ToSingle(Memory.ReadFromAddress(Int64.Parse(Addresses.BaseAddress), Addresses.FOV), 0)
+                    );
                 case "FPS":
-                    return (1000000 / BitConverter.ToInt16(Memory.ReadFromAddress(Addresses.FPS[0]), 0)).ToString();
+                    return String.Format(
+                        "{0:0.00}", 
+                        (1000000 / BitConverter.ToInt16(Memory.ReadFromAddress(Int64.Parse(Addresses.BaseAddress), Addresses.FPS[0]), 0))
+                    );
                 case "DOF":
+                    break;
+                case "RES":
+                    return String.Format(
+                        "{0}x{1}", 
+                        BitConverter.ToInt16(Memory.ReadFromAddress(Int64.Parse(Addresses.BaseAddress), Addresses.ResWidth[0]), 0), 
+                        BitConverter.ToInt16(Memory.ReadFromAddress(Int64.Parse(Addresses.BaseAddress), Addresses.ResHeight[0]), 0)
+                    );
+                case "AR":
                     break;
             }
             return "";
         }
 
-        public static void Set(string grab, float newVal)
+        public static void Set(string grab, float newVal, float temp = 0)
         {
-            switch (grab)
+            switch (grab.ToUpper())
             {
-                #region FOV
+                #region Field Of View
                 case "FOV":
                     if (!newVal.Equals(78))
                         Console.WriteLine("Setting FPS to {0}.", newVal);
@@ -296,7 +372,7 @@ namespace Manager
                     switch (newVal >= 65 && newVal <= 150)
                     {
                         case true:
-                            Memory.WriteToAddress(Addresses.FOV, BitConverter.GetBytes(newVal));
+                            Memory.WriteToAddress(Int64.Parse(Addresses.BaseAddress), Addresses.FOV, BitConverter.GetBytes(newVal));
                             break;
                         case false:
                             if (newVal < 65)
@@ -307,7 +383,7 @@ namespace Manager
                     }
                     break;
                 #endregion
-                #region FPS
+                #region Frames Per Second
                 case "FPS":
                     if (!newVal.Equals(60))
                         Console.WriteLine("Setting FPS to {0}.", newVal);
@@ -318,7 +394,7 @@ namespace Manager
                     {
                         case true:
                             for (int i = 0; i < Addresses.FPS.Count; i++)
-                                Memory.WriteToAddress(Addresses.FPS[i], BitConverter.GetBytes(1000000 / Convert.ToInt16(newVal)));
+                                Memory.WriteToAddress(Int64.Parse(Addresses.BaseAddress), Addresses.FPS[i], BitConverter.GetBytes(1000000 / Convert.ToInt16(newVal)));
                             break;
                         case false:
                             if (newVal < 30)
@@ -329,9 +405,29 @@ namespace Manager
                     }
                     break;
                 #endregion
-                #region DOF
+                #region Depth Of Field
                 case "DOF":
                     break;
+                #endregion
+                #region Resolution
+                /*case "RES":
+                    if (!(newVal.Equals(960) && temp.Equals(540)).Equals(true))
+                        Console.WriteLine("Setting RES to {0}x{1}.", newVal, temp);
+                    else
+                        Console.WriteLine("Setting RES back to default.");
+
+                    switch (newVal >= 960 && temp >= 540)
+                    {
+                        case true:
+                                Memory.WriteToAddress(Int64.Parse(Addresses.BaseAddress), Addresses.ResWidth[3], BitConverter.GetBytes(Convert.ToInt16(newVal)));
+                                Memory.WriteToAddress(Int64.Parse(Addresses.BaseAddress), Addresses.ResHeight[3], BitConverter.GetBytes(Convert.ToInt16(temp)));
+                            break;
+                        case false:
+                            if (newVal < 960 || temp < 540)
+                                Console.WriteLine("Not possible to set RES lower than 960x540.");
+                            break;
+                    }
+                    break;*/
                 #endregion
             }
         }
@@ -373,6 +469,149 @@ namespace Manager
                     else return "Invalid argument.";
             }
             return "";
+        }
+    }
+
+    unsafe class Tls
+    {
+        [Flags]
+        public enum ProcessAccessFlags : uint
+        {
+            All = 0x001F0FFF
+        }
+
+        [Flags]
+        public enum ThreadAccessFlags : uint
+        {
+            All = 0x1F03FF
+        }
+
+        public enum ThreadInfoClass : int
+        {
+            ThreadBasicInformation = 0
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1, Size = 48)]
+        public class ThreadBasicInformation
+        {
+            public uint ExitStatus;
+            public uint _padding;
+            public ulong TebBaseAddress;
+            public ulong ProcessID;
+            public ulong ThreadId;
+            public ulong AffinityMask;
+            public uint Priority;
+            public uint BasePriority;
+        }
+
+        [DllImport("kernel32.dll", EntryPoint = "OpenProcess", SetLastError = true)]
+        private static extern IntPtr UnmanagedOpenProcess(
+            ProcessAccessFlags dwDesiredAccess,
+            bool bInheritHandle,
+            uint dwProcessId
+        );
+
+        [DllImport("kernel32.dll", EntryPoint = "OpenThread", SetLastError = true)]
+        private static extern IntPtr UnmanagedOpenThread(
+            ThreadAccessFlags dwDesiredAccess,
+            bool bInheritHandle,
+            uint dwThreadId
+        );
+
+        [DllImport("kernel32.dll", EntryPoint = "CloseHandle", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnmanagedCloseHandle(IntPtr hObject);
+
+        [DllImport("kernel32.dll", EntryPoint = "ReadProcessMemory", SetLastError = true)]
+        private static extern bool UnmanagedReadProcessMemory(
+            IntPtr hProcess,
+            UIntPtr lpBaseAddress,
+            [Out] byte[] lpBuffer,
+            int dwSize,
+            out int lpNumberOfBytesRead
+        );
+
+        [DllImport("ntdll.dll", EntryPoint = "NtQueryInformationThread", SetLastError = true)]
+        private static extern int UnmanagedNtQueryInformationThread(
+            IntPtr threadHandle,
+            ThreadInfoClass threadInformationClass,
+            IntPtr threadInformation,
+            int threadInformationLength,
+            IntPtr returnLengthPtr
+        );
+
+        public static IntPtr OpenProcess(ProcessAccessFlags access, bool inheritHandle, uint processId)
+        {
+            IntPtr handle = UnmanagedOpenProcess(access, inheritHandle, processId);
+            if (handle == null)
+            {
+                throw new Win32Exception();
+            }
+            return handle;
+        }
+
+        public static IntPtr OpenThread(ThreadAccessFlags access, bool inheritHandle, uint threadId)
+        {
+            IntPtr handle = UnmanagedOpenThread(access, inheritHandle, threadId);
+            if (handle == null)
+            {
+                throw new Win32Exception();
+            }
+            return handle;
+        }
+
+        public static void CloseHandle(IntPtr handle)
+        {
+            if (!UnmanagedCloseHandle(handle))
+            {
+                throw new Win32Exception();
+            }
+        }
+
+        public static void ReadProcessMemory(IntPtr hProcess, UIntPtr lpBaseAddress, [Out] byte[] lpBuffer, int dwSize, out int lpNumberOfBytesRead)
+        {
+            if (!UnmanagedReadProcessMemory(hProcess, lpBaseAddress, lpBuffer, dwSize, out lpNumberOfBytesRead))
+            {
+                throw new Win32Exception();
+            }
+        }
+
+        public static ThreadBasicInformation GetThreadInformation(IntPtr threadHandle)
+        {
+            ThreadBasicInformation info = new ThreadBasicInformation();
+            int size = Marshal.SizeOf(typeof(ThreadBasicInformation));
+            var buf = Marshal.AllocHGlobal(size);
+
+            if (UnmanagedNtQueryInformationThread(threadHandle, ThreadInfoClass.ThreadBasicInformation, buf, size, IntPtr.Zero) != 0)
+            {
+                throw new Win32Exception();
+            }
+
+            Marshal.PtrToStructure(buf, info);
+            Marshal.FreeHGlobal(buf);
+            buf = IntPtr.Zero;
+
+            return info;
+        }
+
+        public static ThreadBasicInformation GetMainHaloThreadInfo()
+        {
+            ThreadBasicInformation info = null;
+
+            var process = Process.GetProcessById(Memory.H5Fpid);
+
+            foreach (ProcessThread thread in process.Threads)
+            {
+                if (thread.ThreadState != System.Diagnostics.ThreadState.Running)
+                    continue;
+
+                var threadHandle = OpenThread(ThreadAccessFlags.All, false, (uint)thread.Id);
+                info = GetThreadInformation(threadHandle);
+                CloseHandle(threadHandle);
+
+                break;
+            }
+            return info;
         }
     }
 }
